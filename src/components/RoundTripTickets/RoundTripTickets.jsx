92c7fe2dae5a3b2a5dc7ce5e0d4c1effe5cdbcc8
MekonenWase <mekonen.wase@gmail.com>
2022-02-12 20:05:19 +0200

    improve design for search result

--- src/components/RoundTripTickets/RoundTripTickets.jsx
+++ src/components/RoundTripTickets/RoundTripTickets.jsx
@@ -31,7 +31,7 @@ const RoundTripTickets = ({ setUserSelectionForRoundTrip }) => {
                     <div className="flight num">
                       <h4>{ticket.flightNumber}</h4>
                       <h5>Departure</h5>
-                        <h5>{ticket.date}</h5>
+                      <h5>{ticket.date}</h5>
                     </div>
                     <div className="depArvTime">
                       <h4>
@@ -102,7 +102,69 @@ const RoundTripTickets = ({ setUserSelectionForRoundTrip }) => {
   return (
     <div className={styles.resultsForRoudTrip}>
       <div className={styles.destination}></div>
-      <div className={styles.results}>{roundTripElements}</div>
+      <div className={styles.results}>
+        <section className={styles.ticketRoundTrip}>
+          <section className={styles.left}>
+            <article className={styles.roundTripTicketsContainer}>
+              <article className={styles.ticketFlightDetailsRoundTrip}>
+                <div className={styles.airlineRoundTrip}>
+                  <img src="" alt="ElAl logo" />
+                  <h4>ElAL</h4>
+                </div>
+                <div className={styles.departureRoundTrip}>
+                  <h5>06:15</h5>
+                  <h4>TLV</h4>
+                </div>
+                <div className={styles.FlightDurationRoundTrip}>
+                  <h5>5h 10m</h5>
+                  <hr />
+                  <h5>non-stop</h5>
+                </div>
+                <div className={styles.arrivalRoundTrip}>
+                  <h5>10:25</h5>
+                  <h4>AMS</h4>
+                </div>
+              </article>
+            </article>
+            <article className={styles.roundTripTicketsContainer}>
+              <article className={styles.ticketFlightDetailsRoundTrip}>
+                <div className={styles.airlineRoundTrip}>
+                  <img src="" alt="ElAl logo" />
+                  <h4>ElAL</h4>
+                </div>
+                <div className={styles.departureRoundTrip}>
+                  <h5>06:15</h5>
+                  <h4>TLV</h4>
+                </div>
+                <div className={styles.FlightDurationRoundTrip}>
+                  <h5>5h 10m</h5>
+                  <hr />
+                  <h5>non-stop</h5>
+                </div>
+                <div className={styles.arrivalRoundTrip}>
+                  <h5>10:25</h5>
+                  <h4>AMS</h4>
+                </div>
+              </article>
+            </article>
+          </section>
+          <section className={styles.right}>
+            <article className={styles.lineSparateRoundTrip}>
+              <div className={styles.topDotRoundTrip}></div>
+              <div className={styles.lineRoundTrip}></div>
+              <div className={styles.bottomDotRoundTrip}></div>
+            </article>
+            <article className={styles.ticketPriceRoundTrip}>
+              <div className={styles.flightPriceRoundTrip}>
+                <h1>$392</h1>
+              </div>
+              <div className={styles.selectBtnRoundTrip}>
+                <button>Select</button>
+              </div>
+            </article>
+          </section>
+        </section>
+      </div>
     </div>
   );
 };
