919c29ebd8eb6ccf6ca809bfd97701f2cd156abc
shpiglify <shpiglify@gmail.com>
2022-02-12 16:14:31 +0200

    use elalRoutes for iata codes

--- setup/helpers.js
+++ setup/helpers.js
@@ -1,18 +1,27 @@
 const IATACodeToCityNameMap = require("./IATACodeToCityNameMap.json");
 const aircrafts = require("./aircrafts.json");
+const elalRoutes = require("../public/data/elalRouts.json")
 
 const _getRandomElement = (array) => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
 const IATACodeToCityName = (IATACode) => {
-  return IATACodeToCityNameMap[IATACode];
+  let city;
+  elalRoutes.forEach(route => {
+    if(route.iata_code === IATACode){
+      city = route.city
+    }
+  })
+  if(!city){
+    const errMsg = `city not found in 'IATACodeToCityName' function for IATA Code ${IATACode}`
+    throw new Error(errMsg)
+  } 
+  return city
 };
 
 const getRandomAircraft = () => {
   return _getRandomElement(aircrafts);
 };
-
-
 
 module.exports = { IATACodeToCityName, getRandomAircraft };
